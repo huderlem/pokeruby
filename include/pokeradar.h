8bb7aa7323b8cd8edc9b584f62c374fdeb1bbaff
Marcus Huderle <huderlem@gmail.com>
2018-06-04 09:05:19 -0500

    Add pokeradar struct from diamond, and generate patch coords

--- include/pokeradar.h
+++ include/pokeradar.h
@@ -3,7 +3,26 @@
 
 #define NUM_POKE_RADAR_GRASS_PATCHES 4
 
-extern s16 gPokeRadarGrassCoords[4][2];
+struct PokeRadarGrassPatch {
+    s16 x;
+    s16 y;
+    u8 patchType;
+    u8 active;
+    u8 continueChain;
+    u8 isShiny;
+};
+
+struct PokeRadarChain {
+    u8 patchType;
+    u8 streak;
+    u16 species;
+    u8 level;
+    u8 active;
+    u8 increasedRates;
+    struct PokeRadarGrassPatch grassPatches[NUM_POKE_RADAR_GRASS_PATCHES];
+};
+
+extern struct PokeRadarChain gPokeRadarChain;
 
 bool8 CanUsePokeRadar(u8 taskId);
 void ItemUseOnFieldCB_PokeRadar(u8 taskId);
--- src/poke_radar.c
+++ src/poke_radar.c
@@ -3,25 +3,46 @@
 #include "field_player_avatar.h"
 #include "field_effect.h"
 #include "fieldmap.h"
+#include "global.fieldmap.h"
 #include "item_use.h"
 #include "metatile_behavior.h"
 #include "pokeradar.h"
+#include "random.h"
 #include "script.h"
 #include "constants/field_effects.h"
+#include "constants/species.h"
 
 static void WaitForShakingPokeRadarGrass(u8 taskId);
 static void FinishPokeRadar(u8 taskId);
 
+bool8 IsValidPokeRadarMetatile(s16 x, s16 y)
+{
+    u16 tileBehavior;
+    s16 gridX, gridY;
+
+    // Must not lie in the map's border.
+    gridX = x - 7;
+    gridY = y - 7;
+    if (gridX < 0 || gridY < 0 || gridX >= gMapHeader.mapData->width || gridY >= gMapHeader.mapData->height)
+    {
+        return FALSE;
+    }
+
+    // Must be tall grass.
+    tileBehavior = MapGridGetMetatileBehaviorAt(x, y);
+    if (!MetatileBehavior_IsTallGrass(tileBehavior))
+        return FALSE;
+
+    return TRUE;
+}
+
 bool8 CanUsePokeRadar(u8 taskId)
 {
     s16 x, y;
-    u16 tileBehavior;
 
+    // Check if the player is standing in a valid pokeradar metatile.
     PlayerGetDestCoords(&x, &y);
-    tileBehavior = MapGridGetMetatileBehaviorAt(x, y);
-
-    // Must be standing in tall grass.
-    if (!MetatileBehavior_IsTallGrass(tileBehavior))
+    if (!IsValidPokeRadarMetatile(x, y))
     {
         DisplayDadsAdviceCannotUseItemMessage(taskId, gTasks[taskId].data[2]);
         return FALSE;
@@ -33,18 +54,121 @@ bool8 CanUsePokeRadar(u8 taskId)
     return TRUE;
 }
 
-#define tWaitDuration data[0]
+static const u8 sNumPatchesInRing[] = { 32, 24, 16, 8, };
 
-void ChoosePokeRadarShakeCoords(s16 baseX, s16 baseY)
+static const s8 sFourthRingCoords[][2] = {
+    {-4, -4},{-4, -3},{-4, -2},{-4, -1},{-4,  0},{-4,  1},{-4,  2},{-4,  3},{-4,  4},
+    {-3, -4},                                                               {-3,  4},
+    {-2, -4},                                                               {-2,  4},
+    {-1, -4},                                                               {-1,  4},
+    { 0, -4},                                                               { 0,  4},
+    { 1, -4},                                                               { 1,  4},
+    { 2, -4},                                                               { 2,  4},
+    { 3, -4},                                                               { 3,  4},
+    { 4, -4},{ 4, -3},{ 4, -2},{ 4, -1},{ 4,  0},{ 4,  1},{ 4,  2},{ 4,  3},{ 4,  4},
+};
+
+static const s8 sThirdRingCoords[][2] = {
+    {-3, -3},{-3, -2},{-3, -1},{-3,  0},{-3,  1},{-3,  2},{-3,  3},
+    {-2, -3},                                             {-2,  3},
+    {-1, -3},                                             {-1,  3},
+    { 0, -3},                                             { 0,  3},
+    { 1, -3},                                             { 1,  3},
+    { 2, -3},                                             { 2,  3},
+    { 3, -3},{ 3, -2},{ 3, -1},{ 3,  0},{ 3,  1},{ 3,  2},{ 3,  3},
+};
+
+static const s8 sSecondRingCoords[][2] = {
+    {-2, -2},{-2, -1},{-2,  0},{-2,  1},{-2,  2},
+    {-1, -2},                           {-1,  2},
+    { 0, -2},                           { 0,  2},
+    { 1, -2},                           { 1,  2},
+    { 2, -2},{ 2, -1},{ 2,  0},{ 2,  1},{ 2,  2},
+};
+
+static const s8 sFirstRingCoords[][2] = {
+    {-1, -1},{-1,  0},{-1,  1},
+    { 0, -1},         { 0,  1},
+    { 1, -1},{ 1,  0},{ 1,  1},
+};
+
+typedef const s8 (*RingCoords)[2];
+static const RingCoords sPokeRadarRingCoords[] = {
+    sFourthRingCoords,
+    sThirdRingCoords,
+    sSecondRingCoords,
+    sFirstRingCoords,
+};
+
+static bool8 SetPokeRadarShakeCoords(s16 baseX, s16 baseY)
 {
-    gPokeRadarGrassCoords[0][0] = baseX - 1;
-    gPokeRadarGrassCoords[0][1] = baseY - 1;
-    gPokeRadarGrassCoords[1][0] = baseX + 1;
-    gPokeRadarGrassCoords[1][1] = baseY - 1;
-    gPokeRadarGrassCoords[2][0] = baseX - 1;
-    gPokeRadarGrassCoords[2][1] = baseY + 1;
-    gPokeRadarGrassCoords[3][0] = baseX + 1;
-    gPokeRadarGrassCoords[3][1] = baseY + 1;
+    int i;
+    bool8 valid;
+
+    for (i = 0; i < NUM_POKE_RADAR_GRASS_PATCHES; i++)
+    {
+        u16 index = (Random() % sNumPatchesInRing[i]);
+        gPokeRadarChain.grassPatches[i].x = baseX + sPokeRadarRingCoords[i][index][0];
+        gPokeRadarChain.grassPatches[i].y = baseY + sPokeRadarRingCoords[i][index][1];
+    }
+
+    // Check to see if at least one coord is in grass.
+    valid = FALSE;
+    for (i = 0; i < NUM_POKE_RADAR_GRASS_PATCHES; i++)
+    {
+        if (IsValidPokeRadarMetatile(gPokeRadarChain.grassPatches[i].x, gPokeRadarChain.grassPatches[i].y))
+        {
+            valid = TRUE;
+            break;
+        }
+    }
+
+    return valid;
+}
+
+static const u8 sNormalPatchRates[] = { 88, 68, 48, 28, };
+static const u8 sHighPatchRates[]   = { 98, 78, 58, 38, };
+
+static bool8 CheckPatchContinuesChain(int patchIndex, u8 increasedRates)
+{
+    const u8 *rates = increasedRates ? sHighPatchRates : sNormalPatchRates;
+    return (Random() % 100) < rates[patchIndex];
+}
+
+static u8 CheckShinyPatch(u8 streak)
+{
+    int shinyChance;
+
+    if (streak == 0)
+        return 0;
+
+    if (streak > 40)
+        streak = 40;
+
+    shinyChance = (65535 / (8200 - streak * 200)) + 1;
+    return Random() < shinyChance;
+}
+
+static void PrepGrassPatchChainData(void)
+{
+    int i;
+    for (i = 0; i < NUM_POKE_RADAR_GRASS_PATCHES; i++)
+    {
+        if (gPokeRadarChain.grassPatches[i].active)
+        {
+            gPokeRadarChain.grassPatches[i].continueChain = CheckPatchContinuesChain(i, gPokeRadarChain.increasedRates);
+            if (gPokeRadarChain.grassPatches[i].continueChain)
+            {
+                gPokeRadarChain.grassPatches[i].patchType = gPokeRadarChain.patchType;
+                gPokeRadarChain.grassPatches[i].isShiny = CheckShinyPatch(gPokeRadarChain.streak);
+            }
+            else
+            {
+                gPokeRadarChain.grassPatches[i].patchType = (Random() % 100 < 50);
+                gPokeRadarChain.grassPatches[i].isShiny = 0;
+            }
+        }
+    }
 }
 
 void StartPokeRadarGrassShake(void)
@@ -52,22 +176,38 @@ void StartPokeRadarGrassShake(void)
     int i;
     struct EventObject *playerObj;
 
+    gPokeRadarChain.active = 1;
+
     playerObj = &gEventObjects[gPlayerAvatar.eventObjectId];
-    ChoosePokeRadarShakeCoords(playerObj->currentCoords.x, playerObj->currentCoords.y);
+    if (!SetPokeRadarShakeCoords(playerObj->currentCoords.x, playerObj->currentCoords.y))
+    {
+        // End pokeradar chain because no grass shook.
+        gPokeRadarChain.species = SPECIES_NONE;
+        gPokeRadarChain.streak = 0;
+        gPokeRadarChain.active = 0;
+        return;
+    }
+
+    PrepGrassPatchChainData();
 
     for (i = 0; i < NUM_POKE_RADAR_GRASS_PATCHES; i++)
     {
-        gFieldEffectArguments[0] = gPokeRadarGrassCoords[i][0];
-        gFieldEffectArguments[1] = gPokeRadarGrassCoords[i][1];
-        gFieldEffectArguments[2] = playerObj->previousElevation;
-        gFieldEffectArguments[3] = 2;
-        gFieldEffectArguments[4] = playerObj->localId << 8 | playerObj->mapNum;
-        gFieldEffectArguments[5] = playerObj->mapGroup;
-        gFieldEffectArguments[6] = (u8)gSaveBlock1.location.mapNum << 8 | (u8)gSaveBlock1.location.mapGroup;
-        gFieldEffectArguments[7] = 0;
-        FieldEffectStart(FLDEFF_TALL_GRASS);
+        if (IsValidPokeRadarMetatile(gPokeRadarChain.grassPatches[i].x, gPokeRadarChain.grassPatches[i].y))
+        {
+            gFieldEffectArguments[0] = gPokeRadarChain.grassPatches[i].x;
+            gFieldEffectArguments[1] = gPokeRadarChain.grassPatches[i].y;
+            gFieldEffectArguments[2] = playerObj->previousElevation;
+            gFieldEffectArguments[3] = 2;
+            gFieldEffectArguments[4] = playerObj->localId << 8 | playerObj->mapNum;
+            gFieldEffectArguments[5] = playerObj->mapGroup;
+            gFieldEffectArguments[6] = (u8)gSaveBlock1.location.mapNum << 8 | (u8)gSaveBlock1.location.mapGroup;
+            gFieldEffectArguments[7] = 0;
+            FieldEffectStart(FLDEFF_TALL_GRASS);
+        }
     }
 }
+
+#define tWaitDuration data[0]
 
 void ItemUseOnFieldCB_PokeRadar(u8 taskId)
 {
